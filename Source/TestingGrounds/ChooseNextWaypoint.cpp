b7f823d74c8ee216cec7f32fa05473f9d5b12a67
Alexandru Stanciu <stanciu.alex74@gmail.com>
2018-01-06 16:53:39 +0000

    Read IndexKeys in C++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,9 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
-EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory) 
 {
-	UE_LOG(LogTemp, Warning, TEXT("Donkey!"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %d"), Index);
 	return EBTNodeResult::Succeeded;
 }
