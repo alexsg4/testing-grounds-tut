2e7e198f4518e95412d8b623a07d6357357b5061
Alexandru Stanciu <stanciu.alex74@gmail.com>
2018-03-06 21:04:57 +0000

    Add Patrol Route component to ThirdPerson Characters and reparent ThirdPerson BP class. Deprecate Patrolling Character class.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,21 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "PatrollingGuard.h" //TODO Remove coupling
 #include "AIController.h"
+#include "TP_ThirdPerson/PatrolRouteComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
+
+
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory) 
 {
+
+	//TODO Protect against no Patrol Route component
+
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	
 	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->GetWaypoints();
 
-	if (!PatrolPoints.Num()) { return EBTNodeResult::Failed; }
+	//Get patrol route
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRouteComponent>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetWaypoints();
+	if (!PatrolPoints.Num()) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points!"));
+	}
 
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 	
